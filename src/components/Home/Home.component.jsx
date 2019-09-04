ce354ff6519ab73550e0f6de910965af856c324b
YaserMJ <yasermuhammedjaser@gmail.com>
2019-09-04 17:22:16 +0300

    version 1

---
+++ src/components/Home/Home.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
+import './Home.styles.css'
+import About from './About.component';
+
+
+const Home = () => (
+  <ReactFullpage
+    licenseKey = {'YOUR_KEY_HERE'}
+    scrollingSpeed = {1000}
+
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section bk">
+            <div>
+            <img className='pkLogo' src="http://pluspng.com/img-png/pokemon-logo-png-pokemon-logo-png-2000.png" alt=""/>
+            </div>
+          </div>
+          <div className="section AboutColor">
+            <About/>
+          </div>
+          <div className="section bkb ">
+            <h2>HEHE 3</h2>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
+
+export default Home;
